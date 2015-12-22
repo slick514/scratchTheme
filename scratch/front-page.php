ac04a288e8a7abe3278bd37ab62d72f5b5d33ade
jgries <justin.gries@gmail.com>
2015-12-22 15:21:51 -0800

    Added widget-display functionality to the front-page

--- scratch/front-page.php
+++ scratch/front-page.php
@@ -8,4 +8,28 @@
  *  @license     GPL-2.0+
  */
 
+function scratch_home_page_setup(){
+    define('HOME_WELCOME_KEY', 'home_welcome');
+    define('CALL_TO_ACTION_KEY', 'call_to_action');
+    
+    // Check the activity of sidebars
+    $home_sidebars = array(
+        HOME_WELCOME_KEY    => is_active_sidebar('home-welcome'),
+        CALL_TO_ACTION_KEY  => is_active_sidebar('call-to-action'),
+    );
+    
+    // Return early if no sidebars are active
+    if(!in_array(true, $home_sidebars)) return;
+    
+    // If the home-welcome widget is active, display it
+    if($home_sidebars[HOME_WELCOME_KEY]){
+        
+    }
+    
+    // if the call-to-action widget is active, display it
+    if($home_sidebars[CALL_TO_ACTION_KEY]){
+        
+    }
+}
+
 genesis();
