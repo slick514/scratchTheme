061864ea3e047d80fa0facbb4c27690214c13852
jgries <justin.gries@gmail.com>
2015-12-22 14:06:59 -0800

    added comments to functions.php code-blocks

--- scratch/functions.php
+++ scratch/functions.php
@@ -20,12 +20,19 @@ add_action('genesis_setup', 'scratch_setup');
  *  @since 1.0.0
  */
 function scratch_setup(){
+    
+    // define theme constants
     define('CHILD_THEME_NAME', 'Scratch');
     define('CHILD_THEME_URL', 'http://github.com/slick514/scratchTheme');
     define('CHILD_THEME_VERSION', '1.0.0');
     
+    // add support for HTML5
     add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption'  ) );
+    
+    // add viewport meta tag for mobile browsers
     add_theme_support( 'genesis-responsive-viewport');
+    
+    // add accessibility support
     add_theme_support( 'genesis-accessibility', array(
         '404-page',
         'drop-down-menu',
